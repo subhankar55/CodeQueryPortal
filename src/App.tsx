13ee9b8ece22043ee89b89e11811aa912d721536
Subhankar <maitisubhankar55@gmail.com>
2025-12-18 22:38:43 +0530

    commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen">
+      <h1 className="text-3xl font-bold underline text-center p-4">
+        Code Query Portal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
